a5c3b795c144e2e3929aa90f4ec886ef39f28529
RiconCla <cspirogone@yande.ru>
2023-06-15 21:51:12 +0300

    fix openPopup and html

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,8 @@ const closePopupButton = document.querySelector('.popup__button-close');
 let formElement = document.querySelector('.popup__form');
 
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_edit-profile-name');
-let descriptionInput = document.querySelector('.popup__input_edit-profile-description');
+let nameInput = document.querySelector('.popup__input_edit_profile-name');
+let descriptionInput = document.querySelector('.popup__input_edit_profile-description');
 
 // Выбераем элементы, куда должны быть вставлены значения полей
 let profileName = document.querySelector('.profile__name');
@@ -24,9 +24,6 @@ function handleFormSubmit (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
     // Получаем значение полей jobInput и nameInput из свойства value
-    //nameInput.value;
-    //descriptionInput.value;
-
     // Вставляем новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileDescription.textContent = descriptionInput.value;
@@ -41,11 +38,14 @@ formElement.addEventListener('submit', handleFormSubmit);
 //Открываем попап
 function openPopup () {
     popupElement.classList.add('popup_opened');
+    //Берем значения из профиля и  кладем в инпуты
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
 }
+
 //Закрываем попап
 function closePopup () {
     popupElement.classList.toggle('popup_opened');
-    console.log("close")
 }
 
 // По клику на кнопку "карандаш" в профиле, открывает попап
